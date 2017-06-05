ae6e3b72705f8226bf4412d741e4dd2978c4242a
armani2k20 <armani2k20@gmail.com>
2017-06-05 16:03:52 +0000

    early worksheet commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,51 @@
+
+
 /**
   * Created by Administrator on 05/06/2017.
   */
 object Main {
 
+  def main(args: Array[String]){
+    // Hello World
+    // Output “Hello World!” via the println() method
+    println("Hello World 1")
 
-  def main(args: Array[String]) = {
-    println("Hello World")
-  }
+    // Assignment
+    // Store “Hello World!” in a variable and then output it.
+    var msg: String = "Hello World 2"
+    println(msg)
 
-}
+    // Parameters
+    // Create a method that accepts a string as a parameter, and then outputs that string.
+    def message(msg: String): Unit ={
+      println(msg)
+    }
+    message("Hello World 3")
+
+    // Return types
+    // Create a method that returns a string, in this case “Hello World!”, which you then use to output.
+    def message2(msg: String): String ={
+      return msg
+    }
+    println(message2("Hello World 4"))
+
+    //Type Inference
+    // Create a method that does not signify a return type and outputs whatever is given to it, then
+    // test it by passing it the follow different data types, Double, String, Integer, Boolean
+    def message3(msg: String): Unit ={
+//      var msg: Boolean = true
+//      var msg: Double = 1.1
+//      var msg: Int = 1
+      var msg: String = "Hello world 5"
+      println(msg)
+    }
+    message3(msg)
+
+
+
+
+
+
+  }// end of main method
+
+}// end of main object
