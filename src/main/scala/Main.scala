cef070c3c2c2beeb11fa33f749a7376b67c30aad
armani2k20 <armani2k20@gmail.com>
2017-06-06 14:26:54 +0000

    Tuesday 6th June 2017: commit 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -186,6 +186,17 @@ object Main {
       case _ => "what?"
     }
     println(patternMatch2(0, 0, true))
+    // ALTERNATIVE WAY BELOW
+
+//    def patternMatch2(n1: Int, n2: Int, q: Boolean): Any = (n1, n2, q) match{
+//      case (n1, n2, q) if n1 == 0 && n2 == 0 => "both " + 0
+//      case (n1,n2, q) if n1 == 0 => "n2: " + n2
+//      case (n1, n2, q) if n2 == 0 => "n1: " + n1
+//      case _ => "what?"
+//    }
+//    println(patternMatch2(1, 0, true))
+
+
 
 
 
--- src/pattern matching.sc
+++ src/pattern matching.sc
@@ -1,10 +1,10 @@
 
-def patternMatch2(input:Any): Any = input match{
+def patternMatch2(n1: Int, n2: Int, q: Boolean): Any = (n1, n2, q) match{
   case (n1, n2, q) if n1 == 0 && n2 == 0 => "both " + 0
   case (n1,n2, q) if n1 == 0 => "n2: " + n2
   case (n1, n2, q) if n2 == 0 => "n1: " + n1
   case _ => "what?"
 }
-println(patternMatch2(0, 1, true))
+println(patternMatch2(1, 0, true))
 
 
